95fb026e304ef1c7856dbb5f53d2e3055aee7016
Frankzhac <Frank.hemle@gmail.com>
2019-06-21 13:32:55 -0400

    app.js and index.js refactored to reflect the client app needs

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, NavLink, withRouter } from 'react-router-dom';
+import Register from './components/register';
+
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <header>
+          <nav>
+            <NavLink to="/register">Register</NavLink>
+            <button onClick={this.logout}>Logout</button>
+          </nav>
+        </header>
+        <main>
+          <Route path="/register" component={Register} />
+        </main>
+      </>
+    );
+  }
+  logout = () => {
+    localStorage.removeItem('jwt');
+
+    this.props.history.push('/login');
+  };
+
 }
 
-export default App;
+export default withRouter(App);
